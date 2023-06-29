68b86d7df36762b21c7581837e4a58b378d3fd26
Raj Singh <raj.singh@intersystems.com>
2023-06-29 15:43:35 -0400

    display config file

--- src/app/datapreview/datapreview.component.ts
+++ src/app/datapreview/datapreview.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-datapreview',
   templateUrl: './datapreview.component.html',
   styles: [
-    '#datapreview { background-color: #444444; height: 800px;}'
+    '#datapreview { color: #bbb; background-color: #444; height: 800px; margin-left: 0px;}',
+    '#previewwin { height: 95%; }',
+    '#datapreview pre { font-size: 9pt; overflow-y: auto; scrollbar-color: #222 #000; scrollbar-width: thin; max-height: 100%; }',
   ]
 })
 export class DatapreviewComponent {
-
+  @Input() config: any;
 }
